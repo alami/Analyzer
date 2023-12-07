f73803ac6f993ad2debfb982f259d45b649bf607
tas_admin <tas_admin@hq.int.unesco.org>
2023-12-07 14:59:23 +0500

    add controllers for 3 roles

---
+++ Analyzer/Controllers/AnalyzerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Analyzer.Controllers
+{
+    public class AnalyzerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Analyzer/Controllers/TesterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Analyzer.Controllers
+{
+    public class TesterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
