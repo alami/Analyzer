ac00d96bc5a95921a8061f410497af12d6d1d9ce
tas_admin <tas_admin@hq.int.unesco.org>
2023-12-09 20:26:39 +0500

    add Device-Controller,Views-index,create

---
+++ Analyzer/Controllers/DeviceController.cs
@@ -0,0 +1,88 @@
+﻿using Analyzer.Data;
+using Analyzer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Analyzer.Controllers
+{
+    public class DeviceController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public DeviceController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Device> objList = _db.Device;
+            return View(objList);
+        }
+        //GET Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //POST Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Device obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Device.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0){
+                return NotFound();
+            }
+            var obj=_db.Device.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        //POST Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Device obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Device.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0){
+                return NotFound();
+            }
+            var obj=_db.Device.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        //POST Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Device.Find(id);
+            if (obj==null) {
+                return NotFound();
+            }
+            _db.Device.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
