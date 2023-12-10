3ebc6eba641024f7d3e4ebf444a060d944473b89
tas_admin <tas_admin@hq.int.unesco.org>
2023-12-10 15:14:38 +0500

    upd Component with Comment Column

---
+++ Analyzer/Data/Migrations/20231210095313_updComponentToDb_CommentCol.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Analyzer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class updComponentToDb_CommentCol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Comment",
+                table: "Component",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Comment",
+                table: "Component");
+        }
+    }
+}
--- Analyzer/Models/Component.cs
+++ Analyzer/Models/Component.cs
@@ -13,6 +13,7 @@ namespace Analyzer.Models
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
+        public string? Comment { get; set; }
         public int? Price { get; set; }
         public int? Time { get; set; }
 
