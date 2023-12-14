447aa23c1d7c7a785ae88bc9d806cf8441add22b
tas_admin <tas_admin@hq.int.unesco.org>
2023-12-14 18:07:36 +0500

    add migration updComponentVisible

---
+++ Analyzer/Data/Migrations/20231213074519_updComponentVisible.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Analyzer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class updComponentVisible : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Visible",
+                table: "Component",
+                type: "bit",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Visible",
+                table: "Component");
+        }
+    }
+}
